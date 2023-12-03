f092367a32fbcf8067b818607cf87dab2d62ec7b
dgarzonmoreno <dxnielgxrzon@gmail.com>
2023-12-03 10:54:29 -0500

    current progress

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -5,16 +5,35 @@ import {
 	MessageList
 } from '@chatscope/chat-ui-kit-react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
-
-import type { ReactElement } from 'react'
+import { useChat } from '@chatscope/use-chat'
+import { ReactElement, useState } from 'react'
 
 export default function ChatWindow(): ReactElement {
+	const [value, setValue] = useState('')
+	const { sendMessage } = useChat()
+
+	const handleSend = (text: any): void => {
+		// Call your API here
+		// Then send the message
+		sendMessage(text)
+	}
+
+	const updateSetValueState = (text: any): void => {
+		const regex = /<br>/g
+		const result = text.replace(regex, '')
+		setValue(result)
+	}
+
 	return (
 		<div className='absolute bottom-0 w-full'>
 			<MainContainer>
 				<ChatContainer className='h-[5000px]'>
 					<MessageList>{/* Your messages here */}</MessageList>
-					<MessageInput />
+					<MessageInput
+						value={value}
+						onSend={handleSend}
+						onChange={updateSetValueState}
+					/>
 				</ChatContainer>
 			</MainContainer>
 		</div>
