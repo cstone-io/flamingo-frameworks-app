097084ab1ce3f7a7658ea46cfa983abdcc21a449
dgarzonmoreno <dxnielgxrzon@gmail.com>
2023-12-05 19:54:58 -0500

    latest changes

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,6 +1,8 @@
 import {
 	ChatContainer,
 	MainContainer,
+	Message,
+	MessageGroup,
 	MessageInput,
 	MessageList
 } from '@chatscope/chat-ui-kit-react'
@@ -10,20 +12,27 @@ import { ReactElement, useState } from 'react'
 
 export default function ChatWindow(): ReactElement {
 	const [value, setValue] = useState('')
-	const { sendMessage, setCurrentMessage } = useChat()
+	const { sendMessage, setCurrentMessage, currentMessages } = useChat()
 
 	const handleSend = (text: string): void => {
 		// Call your API here
 		// Then send the message
-		console.log('text: ' + text)
-		setCurrentMessage(value)
+		// const input = new ChatMessage<MessageContentType>({
+		// 	id: '1',
+		// 	contentType: 0,
+		// 	senderId: '123',
+		// 	direction: MessageDirection.Outgoing,
+		// 	content: MessageContentTypeTextContent,
+		// 	createdTime: ,
+		// })
+		setCurrentMessage(text)
+		console.log('prompt that was submitted to API: \n' + text)
 		setValue('')
 	}
 
 	const updateSetValueState = (text: string): void => {
 		const regex = /<br>/g
 		const result = text.replace(regex, '')
-		console.log(result)
 		setValue(result)
 	}
 
@@ -31,7 +40,23 @@ export default function ChatWindow(): ReactElement {
 		<div className='absolute bottom-0 w-full'>
 			<MainContainer>
 				<ChatContainer className='h-[5000px]'>
-					<MessageList>{/* Your messages here */}</MessageList>
+					<MessageList>
+						{currentMessages.map(group => (
+							<MessageGroup key={group.id} direction={group.direction}>
+								{group.messages.map(message => (
+									<Message
+										key={message.id}
+										model={{
+											type: 'text',
+											payload: message.content,
+											direction: 'outgoing',
+											position: 'last'
+										}}
+									/>
+								))}
+							</MessageGroup>
+						))}
+					</MessageList>
 					<MessageInput
 						value={value}
 						onSend={handleSend}
